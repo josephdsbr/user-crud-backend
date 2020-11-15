2943721139ec3ba13b1381e1c02e8f5392eae7d9
josephdsbr <josephdsbr@gmail.com>
2020-11-15 17:28:25 -0300

    alterando estrutura de retorno da requisicao para enviar o erro

--- src/useCases/AuthUser/AuthUserController.ts
+++ src/useCases/AuthUser/AuthUserController.ts
@@ -12,9 +12,9 @@ export class AuthUserController {
         try {
             const params: IAuthUserDTO = request.body;
             const user: UserWithTokenModel = await this.authUserUseCase.execute(params);
-            return response.status(200).send(user);
+            return response.sendStatus(200).send(user);
         } catch (err) {
-            return response.status(200).json({ error: err.message || 'Unexpected Error' })
+            return response.sendStatus(400).json({ error: err.message || 'Unexpected Error' })
         }
     }
 }
